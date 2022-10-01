51281ca39898edb51042865b7b3d6bde38a9981c
rogNobara <cayorch@gmail.com>
2022-09-30 23:44:22 -0500

    still learning

---
+++ Python/Python Developer 2022/webScraping/scraping.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+res = requests.get('https://news.ycombinator.com/')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = (soup.select('.titleline'))
+votes = (soup.select('.score'))
+
+def create_custom_hackerNews(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+
+        title =links[idx].getText()
+        href = links[idx].find('a').get('href', None)
+        points = int(votes[idx].getText().replace(' points', ''))
+        hn.append({'title': title, 'link': href })
+    return hn
+
+print(create_custom_hackerNews(links, votes))
+
